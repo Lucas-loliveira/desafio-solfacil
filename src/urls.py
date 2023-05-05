06d2793381106622f62e3e20802ec57bf8a8261c
LucasPIB <lucasoliveira783@gmail.com>
2023-05-05 15:51:46 -0300

    feat: setup django and docker

---
+++ src/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers
+from partners.views import ImportPartnerViewSet,PartnerViewSet, upload_file
+
+router = routers.DefaultRouter()
+router.register(r'api/import-partners', ImportPartnerViewSet, basename="import-partners")
+router.register(r'api/partners', PartnerViewSet, basename="partners")
+
+urlpatterns = [
+     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('upload/', upload_file, name='upload_file'),
+]
